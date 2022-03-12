f4a95c348b5add118c7e1b61919c967a1c297c82
Alastair M. Robinson <robinsonb5@gmail.com>
2022-03-12 11:40:49 +0000

    Adjust QMTech Cyclone V SDRAM phase.

--- PLL/CycloneV_50_100/pll/pll_0002.v
+++ PLL/CycloneV_50_100/pll/pll_0002.v
@@ -26,7 +26,7 @@ module  pll_0002(
 		.operation_mode("direct"),
 		.number_of_clocks(3),
 		.output_clock_frequency0("100.000000 MHz"),
-		.phase_shift0("-1500 ps"),
+		.phase_shift0("-3000 ps"),
 		.duty_cycle0(50),
 		.output_clock_frequency1("100.000000 MHz"),
 		.phase_shift1("0 ps"),
